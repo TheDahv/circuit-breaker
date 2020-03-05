2b5d43138eee728f3e537771a1fddd7a7c9f4536
David Pierce <david.dean.pierce@gmail.com>
2020-03-05 11:59:29 -0800

    Add shutdown method for active Manager instances

--- src/manager.ts
+++ src/manager.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from 'express'
 import { Dependency, isHealthy } from './dependency'
 
 export class Manager {
-  private static instance: Manager
+  private static instance?: Manager
 
   // Prevent creating a manager with "new"
   private constructor () {}
@@ -51,5 +51,12 @@ export class Manager {
       }, dependency.intervalMs)
       this.schedules.set(dependency.name, scheduleId)
     }
+  }
+
+  public shutdown () {
+    this.schedules.forEach(timerId => clearInterval(timerId))
+    this.statusCache.clear()
+    this.dependencies.clear()
+    Manager.instance = undefined
   }
 }
