cae701c1c11f0dfa6fc53a511d505a8a89282b89
David Pierce <david.dean.pierce@gmail.com>
2020-03-12 13:26:51 -0700

    Publish main entry point for package consumers

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * This file organizes exports for package consumers.
+ *
+ * @packageDocumentation
+ */
+
+export { Manager } from './manager'
+export { isHealthy, Datadog } from './dependency'
+export { server } from './server'
